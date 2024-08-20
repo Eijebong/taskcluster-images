5e7ebae033e7e430a528dc1cf921199f94267069
Bastien Orivel <eijebong@bananium.fr>
2024-08-20 20:07:19 +0200

    Fix kaniko path

--- push-image/push_image.sh
+++ push-image/push_image.sh
@@ -17,7 +17,7 @@ export REGISTRY_AUTH_FILE=$HOME/.dockercfg
 echo "=== Preparing docker image ==="
 
 cp -R $MOZ_FETCHES_DIR/* $VCS_PATH/taskcluster/docker/${NAME}
-/kaniko/executor --context dir:///$VCS_PATH/taskcluster/docker/${NAME} \
+/kaniko-bootstrap/executor --context dir:///$VCS_PATH/taskcluster/docker/${NAME} \
     --destination image \
     --dockerfile Dockerfile \
     --no-push --no-push-cache \
