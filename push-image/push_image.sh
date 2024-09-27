8ce758489381266a0674c5f7b04f0f0d6503a403
Bastien Orivel <eijebong@bananium.fr>
2024-09-27 17:18:42 +0200

    Also always push ref-latest

--- push-image/push_image.sh
+++ push-image/push_image.sh
@@ -38,4 +38,5 @@ umoci insert --image ${NAME}:final version.json /version.json
 echo "=== Pushing image ==="
 
 skopeo copy oci:${NAME}:final docker://$DOCKER_REPO:$DOCKER_TAG
+skopeo copy oci:${NAME}:final docker://$DOCKER_REPO:${VCS_HEAD_REF}-latest
 
